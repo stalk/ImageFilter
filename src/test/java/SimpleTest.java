310790d02024fd2b37def34471251ace38a2cd2f
Махмутов Ринат Гумерович <Rinat_Makhmutov@epam.com>
2017-02-02 14:15:00 +0300

    add simple test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Rinat_Makhmutov
+ */
+
+
+public class SimpleTest {
+
+    @Test
+    public void test1(){
+        assertEquals(true,true);
+    }
+}
