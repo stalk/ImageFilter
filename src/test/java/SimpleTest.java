efb54a59697e900aa5d82649942d14d6ade4a187
Махмутов Ринат Гумерович <Rinat_Makhmutov@epam.com>
2017-02-02 14:48:56 +0300

    fix test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * @author Rinat_Makhmutov
@@ -16,6 +18,6 @@ public class SimpleTest {
 
     @Test
     public void test2(){
-        assertEquals(true,false);
+        assertNotEquals(true,false);
     }
 }
